0704ef7f91dded2207785c933c6dce8b5dae0868
Bryan Joseph <bryanjos@gmail.com>
2013-09-10 15:59:49 -0500

    Made stats class

--- src/main/scala/com/bryanjos/lovecouch/CouchDB.scala
+++ src/main/scala/com/bryanjos/lovecouch/CouchDB.scala
@@ -3,21 +3,174 @@ package com.bryanjos.lovecouch
 import scala.concurrent._
 import ExecutionContext.Implicits.global
 import play.api.libs.json._
+import play.api.libs.functional.syntax._
 import dispatch.stream.StringsByLine
 
 case class CouchDb(host: String = "127.0.0.1", port: Int = 5984) {
   def url: String = s"http://$host:$port"
 }
 
+case class CouchDbInfo(couchdb: String, version: String, uuid: String, vendor: Vendor)
+
+case class Vendor(version: String, name: String)
+
+case class ActiveTask(pid: String, status: String, task: String, taskType: String)
+
+case class DatabaseEvent(dbName: String, event:String)
+
+case class ReplicationSpecification(createTarget:Boolean = false, source:String, target:String)
+
+case class ReplicationHistory(docsRead:Long, sessionId:String, recordedSeq:Long,
+                              endLastSeq:Long, docWriteFailures:Long, startTime:String,
+                               startLastSeq:Long, endTime:String, missingChecked:Long,
+                               docsWritten:Long, missingFound:Long)
+
+
+case class ReplicationResponse(ok:Boolean, history:Vector[ReplicationHistory], sessionId:String, sourceLastSeq:Long)
+
+case class StatItem(description:String, current:Option[Long], sum:Option[Long], mean:Option[Long], stddev:Option[Long],
+                    min:Option[Long], max:Option[Long])
+
+
+case class CouchDBStats(authCacheMisses:StatItem, databaseWrites:StatItem, openDatabases:StatItem,
+                        authCacheHits:StatItem, requestTime:StatItem, databaseReads:StatItem, openOSFiles:StatItem)
+
+
+case class HttpRequestMethodStats(PUT:StatItem, GET:StatItem, COPY:StatItem,
+                        DELETE:StatItem, POST:StatItem, HEAD:StatItem)
+
+
+case class HttpStatusCodeStats( _403:StatItem, _202:StatItem, _401:StatItem,
+                                _409:StatItem, _200:StatItem, _405:StatItem,
+                                _400:StatItem, _201:StatItem, _404:StatItem,
+                                _500:StatItem, _412:StatItem, _301:StatItem,
+                                _304:StatItem)
+
+
+case class HttpdStats(clientsRequestingChanges:StatItem, temporaryViewReads:StatItem, requests:StatItem,
+                                  bulkRequests:StatItem, viewReads:StatItem)
+
+case class Stats(couchdb:CouchDBStats, httpdRequestMethods:HttpRequestMethodStats,
+                 httpdStatusCodes:HttpStatusCodeStats, httpd:HttpdStats)
+
+object FeedTypes extends Enumeration {
+  type FeedTypes = Value
+  val LongPoll = Value("longpoll")
+  val Continuous = Value("continuous")
+  val EventSource = Value("eventsource")
+  val Normal = Value("normal")
+}
+
 object CouchDb {
+  implicit val vendorFmt = Json.format[Vendor]
+  implicit val couchDbInfoFmt = Json.format[CouchDbInfo]
+
+  implicit val activeTaskReads = (
+    (__ \ "pid").read[String] ~
+      (__ \ "status").read[String] ~
+      (__ \ "task").read[String] ~
+      (__ \ "type").read[String]
+    )(ActiveTask.apply _)
+
+
+  implicit val databaseEventReads = (
+      (__ \ "dbname").read[String] ~
+      (__ \ "type").read[String]
+    )(DatabaseEvent.apply _)
+
+
+
+  implicit val replicationSpecificationWrites = (
+    (__ \ "create_target").write[Boolean] ~
+      (__ \ "source").write[String] ~
+      (__ \ "target").write[String]
+    )(unlift(ReplicationSpecification.unapply))
+
+
+
+  implicit val replicationHistoryReads = (
+    (__ \ "docs_read").read[Long] ~
+      (__ \ "session_id").read[String] ~
+      (__ \ "recorded_seq").read[Long] ~
+      (__ \ "end_last_seq").read[Long] ~
+      (__ \ "doc_write_failures").read[Long] ~
+      (__ \ "start_time").read[String] ~
+      (__ \ "start_last_seq").read[Long] ~
+      (__ \ "end_time").read[String] ~
+      (__ \ "missing_checked").read[Long] ~
+      (__ \ "docs_written").read[Long] ~
+      (__ \ "missing_found").read[Long]
+
+    )(ReplicationHistory.apply _)
+
+
+  implicit val replicationResponseReads = (
+    (__ \ "ok").read[Boolean] ~
+      (__ \ "history").read[Vector[ReplicationHistory]] ~
+      (__ \ "session_id").read[String] ~
+      (__ \ "source_last_seq").read[Long]
+
+    )(ReplicationResponse.apply _)
+
+
+  implicit val statItemFmt = Json.format[StatItem]
+
+  implicit val couchDBStatsReads = (
+      (__ \ "auth_cache_misses").read[StatItem] ~
+      (__ \ "database_writes").read[StatItem] ~
+      (__ \ "open_databases").read[StatItem] ~
+      (__ \ "auth_cache_hits").read[StatItem] ~
+      (__ \ "request_time").read[StatItem] ~
+      (__ \ "database_reads").read[StatItem] ~
+      (__ \ "open_os_files").read[StatItem]
+    )(CouchDBStats.apply _)
+
+
+  implicit val HttpRequestMethodStatsFmt = Json.format[HttpRequestMethodStats]
+
+
+  implicit val HttpStatusCodeStatsReads = (
+      (__ \ "403").read[StatItem] ~
+      (__ \ "202").read[StatItem] ~
+      (__ \ "401").read[StatItem] ~
+      (__ \ "409").read[StatItem] ~
+      (__ \ "200").read[StatItem] ~
+      (__ \ "405").read[StatItem] ~
+      (__ \ "400").read[StatItem] ~
+      (__ \ "201").read[StatItem] ~
+      (__ \ "404").read[StatItem] ~
+      (__ \ "500").read[StatItem] ~
+      (__ \ "412").read[StatItem] ~
+      (__ \ "301").read[StatItem] ~
+      (__ \ "304").read[StatItem]
+    )(HttpStatusCodeStats.apply _)
+
+
+  implicit val httpdStatsReads = (
+    (__ \ "clients_requesting_changes").read[StatItem] ~
+      (__ \ "temporary_view_reads").read[StatItem] ~
+      (__ \ "requests").read[StatItem] ~
+      (__ \ "bulk_requests").read[StatItem] ~
+      (__ \ "view_reads").read[StatItem]
+    )(HttpdStats.apply _)
+
+
+  implicit val statsRead = (
+    (__ \ "couchdb").read[CouchDBStats] ~
+      (__ \ "httpd_request_methods").read[HttpRequestMethodStats] ~
+      (__ \ "httpd_status_codes").read[HttpStatusCodeStats] ~
+      (__ \ "httpd").read[HttpdStats]
+    )(Stats.apply _)
+
+
   /**
    * Meta information about the instance
    * @param couchDb
    * @return
    */
-  def info()(implicit couchDb: CouchDb = CouchDb()): Future[JsValue] = {
+  def info()(implicit couchDb: CouchDb = CouchDb()): Future[CouchDbInfo] = {
     for(res <- Requests.get(couchDb.url))
-    yield Json.parse(res)
+    yield Json.fromJson[CouchDbInfo](Json.parse(res)).get
   }
 
   /**
@@ -25,9 +178,9 @@ object CouchDb {
    * @param couchDb
    * @return
    */
-  def activeTasks()(implicit couchDb: CouchDb = CouchDb()): Future[JsValue] = {
+  def activeTasks()(implicit couchDb: CouchDb = CouchDb()): Future[Vector[ActiveTask]] = {
     for(res <- Requests.get(couchDb.url + "/_active_tasks"))
-    yield Json.parse(res)
+    yield Json.fromJson[Vector[ActiveTask]](Json.parse(res)).get
   }
 
   /**
@@ -35,9 +188,9 @@ object CouchDb {
    * @param couchDb
    * @return
    */
-  def allDbs()(implicit couchDb: CouchDb = CouchDb()): Future[JsValue] = {
+  def allDbs()(implicit couchDb: CouchDb = CouchDb()): Future[Vector[String]] = {
     for(res <- Requests.get(couchDb.url + "/_all_dbs"))
-    yield Json.parse(res)
+    yield Json.parse(res).as[Vector[String]]
   }
 
   /**
@@ -48,11 +201,11 @@ object CouchDb {
    * @param couchDb
    * @return
    */
-  def updates(feed: String, timeout: Long = 60, heartbeat: Boolean = true,
-                callBack: JsValue => Unit)
+  def updates(feed: FeedTypes.FeedTypes = FeedTypes.LongPoll, timeout: Long = 60, heartbeat: Boolean = true,
+                callBack: DatabaseEvent => Unit)
                (implicit couchDb: CouchDb = CouchDb()): Object with StringsByLine[Unit] = {
-    Requests.getStream(couchDb.url + s"/_db_updates?feed=${feed}&timeout=$timeout&heartbeat=$heartbeat",
-    line => callBack(Json.parse(line)))
+    Requests.getStream(couchDb.url + s"/_db_updates?feed=${feed.toString}&timeout=$timeout&heartbeat=$heartbeat",
+    line => callBack(Json.fromJson[DatabaseEvent](Json.parse(line)).get))
   }
 
   /**
@@ -76,10 +229,11 @@ object CouchDb {
    * @param couchDb
    * @return
    */
-  def replicate(bytes: Long = 1000, offset: Long = 0, replicationSpecification: JsValue)
-               (implicit couchDb: CouchDb = CouchDb()): Future[JsValue] = {
-    for(res <- Requests.post(couchDb.url + s"/_replicate?bytes=$bytes&offset=$offset", body = Json.stringify(replicationSpecification)))
-    yield Json.parse(res)
+  def replicate(bytes: Long = 1000, offset: Long = 0, replicationSpecification: ReplicationSpecification)
+               (implicit couchDb: CouchDb = CouchDb()): Future[ReplicationResponse] = {
+    for(res <- Requests.post(couchDb.url + s"/_replicate?bytes=$bytes&offset=$offset",
+      body = Json.stringify(Json.toJson(replicationSpecification))))
+    yield Json.fromJson[ReplicationResponse](Json.parse(res)).get
   }
 
   /**
@@ -87,9 +241,9 @@ object CouchDb {
    * @param couchDb
    * @return
    */
-  def restart()(implicit couchDb: CouchDb = CouchDb()): Future[JsValue] = {
+  def restart()(implicit couchDb: CouchDb = CouchDb()): Future[Boolean] = {
     for(res <- Requests.post(couchDb.url + s"/_restart", headers = Map("Content-Type" -> "application/json")))
-    yield Json.parse(res)
+    yield (Json.parse(res) \ "ok").as[Boolean]
   }
 
   /**
@@ -97,9 +251,9 @@ object CouchDb {
    * @param couchDb
    * @return
    */
-  def stats()(implicit couchDb: CouchDb = CouchDb()): Future[JsValue] = {
+  def stats()(implicit couchDb: CouchDb = CouchDb()): Future[Stats] = {
     for(res <- Requests.get(couchDb.url + s"/_stats"))
-    yield Json.parse(res)
+    yield Json.fromJson[Stats](Json.parse(res)).get
   }
 
   /**
@@ -108,8 +262,8 @@ object CouchDb {
    * @param count
    * @return
    */
-  def uuids(count: Int = 1)(implicit couchDb: CouchDb = CouchDb()): Future[JsValue] = {
+  def uuids(count: Int = 1)(implicit couchDb: CouchDb = CouchDb()): Future[Vector[String]] = {
     for(res <- Requests.post(couchDb.url + s"/_uuids?count=$count"))
-    yield Json.parse(res)
+    yield (Json.parse(res) \ "uuids").as[Vector[String]]
   }
 }
