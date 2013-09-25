fe2b4d47d9ef57827aff0a71192810a04838b1b1
Bryan Joseph <bryanjos@gmail.com>
2013-09-25 13:44:17 -0500

    More rewritten tests

--- src/test/scala/com/bryanjos/lovecouch/DesignDocumentSpec.scala
+++ src/test/scala/com/bryanjos/lovecouch/DesignDocumentSpec.scala
@@ -7,56 +7,73 @@ import scala.concurrent.Await
 import ExecutionContext.Implicits.global
 import play.api.libs.json.Json
 
-class DesignDocumentSpec extends FunSpec with BeforeAndAfterAll{
-  implicit val db = Database(name="designdocumentspec")
+class DesignDocumentSpec extends FunSpec with BeforeAndAfterAll {
+  implicit val db = Database(name = "designdocumentspec")
   var id = ""
   var revs = List[String]()
-  case class Guy(_id:Option[String] = None, _rev:Option[String] = None, name:String, age:Long)
+
+  case class Guy(_id: Option[String] = None, _rev: Option[String] = None, name: String, age: Long)
+
   implicit val guyFmt = Json.format[Guy]
 
   val dd = DesignDocument(_id = "_design/ages", views = List[View](View(name = "by_age", map = "function(doc) { if (doc.age > 30) { emit(null, doc.age); } }")))
+
+  info("CouchDB Design Document Methods")
 
   override def beforeAll() {
     Await.result(Database.create(), 5 seconds)
   }
 
-  describe("Design Document"){
-    it("should put"){
-      val result = DesignDocument.put(dd) map { value =>
-        id = value.get.id
-        revs = revs ++ List[String](value.get.rev)
-        assert(value.get.ok)
+  describe("Upload the specified design document") {
+    it("should be ok") {
+      val result = DesignDocument.put(dd) map {
+        value =>
+          assert(value.isSuccess)
+          id = value.get.id
+          revs = revs ++ List[String](value.get.rev)
+          assert(value.get.ok)
+      }
+
+      Await.result(result, 5 seconds)
+    }
+  }
+
+  describe("Returns the specified design document") {
+    it("should return the design document") {
+      val result = DesignDocument.get(id) map {
+        value =>
+          assert(value.isSuccess)
+          assert(value.get._id == id)
+          assert(value.get._rev.get == revs.head)
+          assert(value.get.views.head.name == "by_age")
       }
 
       Await.result(result, 5 seconds)
     }
 
-    it("should get"){
-      val result = DesignDocument.get(id) map { value =>
-        assert(value.isSuccess)
-        assert(value.get._id == id)
-        assert(value.get._rev.get == revs.head)
-        assert(value.get.views.head.name == "by_age")
+
+  }
+
+  describe("Executes the specified view-name from the specified design-doc design document.") {
+    it("should return one result") {
+      Await.result(Database.bulkDocs(Seq[Guy](Guy(name = "Alf", age = 23), Guy(name = "SuperAlf", age = 46))), 5 seconds)
+      val result = DesignDocument.executeView("_design/ages", "by_age") map {
+        value =>
+          assert(value.isSuccess)
+          assert(value.get.rows.size == 1)
       }
 
       Await.result(result, 5 seconds)
     }
+  }
 
-    it("should execute"){
-      Await.result(Database.bulkDocs(Seq[Guy](Guy(name="Alf", age=23), Guy(name="SuperAlf", age=46))), 5 seconds)
-      val result = DesignDocument.executeView("_design/ages", "by_age") map { value =>
-        assert(value.isSuccess)
-        assert(value.get.rows.size == 1)
+  describe("Delete an existing design document") {
+    it("should be deleted successfully") {
+      val result = DesignDocument.delete(id, revs.head) map {
+        value =>
+          assert(value.isSuccess)
+          assert(value.get.ok)
       }
-
-      Await.result(result, 5 seconds)
-    }
-
-    it("should delete"){
-      val result = DesignDocument.delete(id, revs.head) map { value =>
-        assert(value.get.ok)
-      }
-
       Await.result(result, 5 seconds)
     }
   }
--- src/test/scala/com/bryanjos/lovecouch/DocumentSpec.scala
+++ src/test/scala/com/bryanjos/lovecouch/DocumentSpec.scala
@@ -5,99 +5,133 @@ import scala.concurrent.{ExecutionContext, Await}
 import ExecutionContext.Implicits.global
 import org.scalatest._
 import play.api.libs.json.Json
-import scala.Some
 
-class DocumentSpec extends FunSpec with BeforeAndAfterAll{
-  implicit val db = Database(name="documentspec")
-  case class Guy(_id:Option[String] = None, _rev:Option[String] = None, name:String, age:Long)
+class DocumentSpec extends FunSpec with BeforeAndAfterAll {
+  implicit val db = Database(name = "documentspec")
+
+  case class Guy(_id: Option[String] = None, _rev: Option[String] = None, name: String, age: Long)
+
   implicit val guyFmt = Json.format[Guy]
   var id = ""
   var revs = List[String]()
+
+  info("CouchDB Document Methods")
 
   override def beforeAll() {
     Await.result(Database.create(), 5 seconds)
   }
 
-  describe("Document"){
-    it("should create a document"){
-
-      val data = Guy(name="Alf", age=23)
-
-      val result = Document.post[Guy](data) map { value =>
-      id = value.get.id
-      revs = revs ++ List[String](value.get.rev)
-      assert(value.get.ok)
-    }
-
-      Await.result(result, 5 seconds)
-    }
-
-    it("should get back the document"){
-      val result = Document.get[Guy](id) map { value =>
-        assert(value.isSuccess)
-        assert(value.get.age == 23)
-        assert(value.get.name == "Alf")
-        assert(value.get._id.get == id)
+  describe("Create a new document") {
+    it("should be created") {
+      val data = Guy(name = "Alf", age = 23)
+      val result = Document.post[Guy](data) map {
+        value =>
+          assert(value.isSuccess)
+          id = value.get.id
+          revs = revs ++ List[String](value.get.rev)
+          assert(value.get.ok)
       }
-
       Await.result(result, 5 seconds)
     }
+  }
 
-    it("should update the document"){
-      val data = Guy(_id = Some(id), _rev = Some(revs.last), name="Alf", age=24)
 
-      val result = Document.put[Guy](data, id) map { value =>
-        revs = revs ++ List[String](value.get.rev)
-        assert(value.get.ok)
+  describe("Returns the latest revision of the document") {
+    it("should be return the wanted document") {
+      val result = Document.get[Guy](id) map {
+        value =>
+          assert(value.isSuccess)
+          assert(value.get.age == 23)
+          assert(value.get.name == "Alf")
+          assert(value.get._id.get == id)
       }
-
       Await.result(result, 5 seconds)
     }
 
 
-    it("should attach a file"){
+  }
 
+
+  describe("Inserts a new document, or new version of an existing document") {
+    it("should be updated") {
+      val data = Guy(_id = Some(id), _rev = Some(revs.last), name = "Alf", age = 24)
+
+      val result = Document.put[Guy](data, id) map {
+        value =>
+          assert(value.isSuccess)
+          revs = revs ++ List[String](value.get.rev)
+          assert(value.get.ok)
+      }
+      Await.result(result, 5 seconds)
+    }
+
+
+  }
+
+
+  describe("Adds an attachment of a document") {
+    it("should be added") {
       val result = Document.putAttachment(id,
         revs.last,
         "README.md",
         new java.io.File("/Users/bryanjos/Projects/Personal/lovecouch/README.md"),
-        "text/plain") map { value =>
-        revs = revs ++ List[String](value.get.rev)
-        assert(value.get.ok)
+        "text/plain") map {
+        value =>
+          assert(value.isSuccess)
+          revs = revs ++ List[String](value.get.rev)
+          assert(value.get.ok)
+      }
+      Await.result(result, 5 seconds)
+    }
+  }
+
+
+  describe("Gets the attachment of a document") {
+    it("should be a byte array with non zero bytes") {
+      val result = Document.getAttachment(id, "README.md") map {
+        value =>
+          assert(value.isSuccess)
+          assert(!value.get.isEmpty)
       }
 
       Await.result(result, 5 seconds)
     }
 
-    it("should get attachment"){
 
-      val result = Document.getAttachment(id, "README.md") map { value =>
-        assert(value.get.size > 0)
+  }
+
+
+  describe("Returns the a revision of the document") {
+    it("should be a previous revision of the document") {
+      val result = Document.get[Guy](id, Some(revs.head)) map {
+        value =>
+          assert(value.isSuccess)
+
+
+          assert(value.get.age == 23)
+          assert(value.get.name == "Alf")
+          assert(value.get._id.get == id)
       }
 
       Await.result(result, 5 seconds)
     }
 
-    it("should get revision of document"){
 
-      val result = Document.get[Guy](id, Some(revs.head)) map { value =>
-        assert(value.isSuccess)
-        assert(value.get.age == 23)
-        assert(value.get.name == "Alf")
-        assert(value.get._id.get == id)
+  }
+
+
+  describe("Deletes the document") {
+    it("should be deleted") {
+      val result = Document.delete(id, revs.last) map {
+        value =>
+          assert(value.isSuccess)
+          assert(value.get.ok)
       }
 
       Await.result(result, 5 seconds)
     }
 
-    it("should delete document"){
 
-      val result = Document.delete(id, revs.last) map { value =>
-        assert(value.get.ok)
-      }
-
-      Await.result(result, 5 seconds)
-    }
   }
 
   override def afterAll() {
